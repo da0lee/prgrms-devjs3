7c3e6b77f712128625206edfb957e2d7fd255ee9
ddayooung <dimmaculatey@gmail.com>
2020-11-08 18:30:04 +0900

    Feat: Autofocus, Reset Input value

--- frontend/src/SearchInput.js
+++ frontend/src/SearchInput.js
@@ -1,10 +1,14 @@
 const TEMPLATE = '<input type="text">';
 class SearchInput {
+  $wrap = null;
+  $searchInput = null;
+
   constructor({ $target, onSearch }) {
     const $wrap = document.createElement('section');
     const $searchInput = document.createElement('input');
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = '고양이를 검색해보세요.|';
+    this.$searchInput.setAttribute('autofocus', 'autofocus');
 
     $searchInput.className = 'SearchInput';
     $target.appendChild($wrap);
@@ -16,6 +20,8 @@ class SearchInput {
         this.recentKeyword.setRecentKeywords(e.target.value);
       }
     });
+
+    $searchInput.addEventListener('click', (e) => (e.target.value = ''));
 
     this.recentKeyword = new RecentKeyword({
       $target,
